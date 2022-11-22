56efbc2a352b53a9bee650e4a7df430464fd717f
Tiphaine DURAND <tidurand@e1r5p17.clusters.42paris.fr>
2022-11-22 16:06:44 +0100

    first commit

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Home() {
+	return (
+	  <div className="Home">
+		<Header />
+		<h1></h1>
+	  </div>
+	);
+  }
+  
+export default Home;
+  
